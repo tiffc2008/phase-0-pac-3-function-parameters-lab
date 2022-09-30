ec909faf92ac93da26f3b77760b21fc2d78f5504
Tiffany Cheng <tiffanycheng.001@gmail.com>
2022-09-30 14:32:06 -0600

    commit tree bc2f0ae9388d9c0c0d5ffcc55f299a21241ca776

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+introduction("Tiffany")
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguage("Tiffany", "spanish")
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguage("Tiffany")
